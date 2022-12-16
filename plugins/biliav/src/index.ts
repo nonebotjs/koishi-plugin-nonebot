4930cb369f0e8680cc397f21d0597bfaf83d9ca5
Shigma <shigma10826@gmail.com>
2022-12-17 01:44:02 +0800

    feat: add test plugin

---
+++ plugins/biliav/src/index.ts
@@ -0,0 +1,10 @@
+import { Context } from 'koishi'
+import {} from 'koishi-plugin-nonebot'
+import { resolve } from 'path'
+
+export const name = 'biliav'
+export const using = ['nonebot']
+
+export function apply(ctx: Context) {
+  ctx.nonebot.import('biliav', resolve(__dirname, '../nonebot_plugin_biliav'))
+}
