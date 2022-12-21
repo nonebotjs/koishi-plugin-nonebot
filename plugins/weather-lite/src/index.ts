a310904c4553b219a79a37894bcade801ef485a9
Shigma <1700011071@pku.edu.cn>
2022-12-22 00:03:38 +0800

    chore: add plugin weather-lite

---
+++ plugins/weather-lite/src/index.ts
@@ -0,0 +1,15 @@
+import { Context, Schema } from 'koishi'
+import {} from 'koishi-plugin-nonebot'
+import { resolve } from 'path'
+
+export const name = 'weather-lite'
+export const using = ['nonebot']
+
+export interface Config {}
+
+export const Config: Schema<Config> = Schema.object({})
+
+export async function apply(ctx: Context, config: Config) {
+  await ctx.nonebot.install(resolve(__dirname, '../dist'))
+  await ctx.nonebot.import(resolve(__dirname, '../nonebot_plugin_weather_lite/nonebot_plugin_weather_lite'), config)
+}
