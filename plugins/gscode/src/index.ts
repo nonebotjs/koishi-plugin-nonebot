971825d9f2b4a8bec10228bd818d7982b59d4a50
Shigma <1700011071@pku.edu.cn>
2022-12-21 19:26:56 +0800

    chore: add plugin gscode

---
+++ plugins/gscode/src/index.ts
@@ -0,0 +1,15 @@
+import { Context, Schema } from 'koishi'
+import {} from 'koishi-plugin-nonebot'
+import { resolve } from 'path'
+
+export const name = 'gscode'
+export const using = ['nonebot']
+
+export interface Config {}
+
+export const Config: Schema<Config> = Schema.object({})
+
+export async function apply(ctx: Context, config: Config) {
+  await ctx.nonebot.install(resolve(__dirname, '../dist'))
+  await ctx.nonebot.import(resolve(__dirname, '../nonebot-plugin-gscode/nonebot-plugin-gscode'), config)
+}
