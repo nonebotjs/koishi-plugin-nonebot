01a36f93e9b87aad578f358faba647c34081144a
Shigma <1700011071@pku.edu.cn>
2022-12-26 00:29:01 +0800

    chore: add plugin simplemusic

--- plugins/today-in-history/src/index.ts
+++ plugins/today-in-history/src/index.ts
@@ -5,9 +5,17 @@ import { resolve } from 'path'
 export const name = 'today-in-history'
 export const using = ['nonebot']
 
-export interface Config {}
+export interface Config {
+  history_qq_friends: number[]
+  history_qq_groups: number[]
+  history_inform_time: string
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  history_qq_friends: Schema.array(Number).description('设定要发送的 QQ 好友。'),
+  history_qq_groups: Schema.array(Number).description('设定要发送的群。'),
+  history_inform_time: Schema.string().description('设定每天发送时间，以空格间隔。').default('7 35'),
+})
 
 export async function apply(ctx: Context, config: Config) {
   await ctx.nonebot.install(resolve(__dirname, '../dist'))
