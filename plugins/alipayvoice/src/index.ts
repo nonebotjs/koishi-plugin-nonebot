af6a7e14902edcb0e18c2d4c80119071db9ff4d5
Shigma <1700011071@pku.edu.cn>
2022-12-29 00:23:16 +0800

    chore: add plugin alipay-voice

---
+++ plugins/alipayvoice/src/index.ts
@@ -0,0 +1,15 @@
+import { Context, Schema } from 'koishi'
+import {} from 'koishi-plugin-nonebot'
+import { resolve } from 'path'
+
+export const name = 'alipayvoice'
+export const using = ['nonebot']
+
+export interface Config {}
+
+export const Config: Schema<Config> = Schema.object({})
+
+export async function apply(ctx: Context, config: Config) {
+  await ctx.nonebot.install(resolve(__dirname, '../dist'))
+  await ctx.nonebot.import(resolve(__dirname, '../nonebot-plugin-alipayvoice/nonebot_plugin_alipayvoice'), config)
+}
