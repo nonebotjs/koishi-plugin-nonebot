c12b0e116f80f1f90132bb63325b64337dfbc8e6
Shigma <1700011071@pku.edu.cn>
2023-01-22 19:44:40 +0800

    chore: add plugin cybersensoji

--- packages/nonebot/python/nonebot/params.py
+++ packages/nonebot/python/nonebot/params.py
@@ -4,6 +4,7 @@ from .internal.params import State as State
 from .internal.params import CommandArg as CommandArg
 from .internal.params import ShellCommandArgs as ShellCommandArgs
 from .internal.params import ShellCommandArgv as ShellCommandArgv
+from .internal.params import Arg as Arg
 from .internal.params import ArgStr as ArgStr
 from .internal.params import ArgPlainText as ArgPlainText
 from .internal.params import RegexDict as RegexDict
