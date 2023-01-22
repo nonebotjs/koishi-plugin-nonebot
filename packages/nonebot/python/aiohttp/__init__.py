fca812bc7c20df2f807d732e2ac0887264113f17
Shigma <1700011071@pku.edu.cn>
2023-01-22 17:58:38 +0800

    feat: support constructor args for aiohttp.ClientSession

--- packages/nonebot/python/aiohttp/__init__.py
+++ packages/nonebot/python/aiohttp/__init__.py
@@ -2,6 +2,13 @@ from pyodide.http import pyfetch
 import json
 
 
+ClientConnectorError = Exception
+
+
+def TCPConnector(ssl=False):
+	pass
+
+
 def request(method, url, data={}, headers={}, timeout=60000):
 	return ClientSession().request(method, url, data, headers, timeout)
 
@@ -13,18 +20,23 @@ class ClientSession:
 	async def __aexit__(self, exc_type, exc_value, traceback):
 		return
 
+	def __init__(self, base_url="", connector=None, trust_env=False):
+		self.base_url = base_url
+		self.connector = connector
+		self.trust_env = trust_env
+
 	def request(self, method, url, data={}, headers={}, timeout=60000):
 		headers["Content-Type"] = "application/json"
 		kwargs = {"method": method, "headers": headers}
 		if method != "GET" and method != "HEAD":
 			kwargs["body"] = json.dumps(data)
-		return ResponseWrapper(pyfetch(url, **kwargs))
+		return ResponseWrapper(pyfetch(self.base_url + url, **kwargs))
 
 	def get(self, url, headers={}, timeout=60000):
-		return self.request("GET", url, headers=headers, timeout=timeout)
+		return self.request("GET", self.base_url + url, headers=headers, timeout=timeout)
 
 	def post(self, url, headers={}, data={}, timeout=60000):
-		return self.request("POST", url, headers=headers, data=data, timeout=timeout)
+		return self.request("POST", self.base_url + url, headers=headers, data=data, timeout=timeout)
 
 
 class ResponseWrapper:
