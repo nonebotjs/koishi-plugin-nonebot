1db504703244a5eb181c4002889a7b970e5286ba
Cyan <68551684+Chinese-Cyq20100313@users.noreply.github.com>
2023-06-10 15:12:38 +0800

    test: update test plugin

--- packages/nonebot/python/nonebot/internal/__init__.pyi
+++ packages/nonebot/python/nonebot/internal/__init__.pyi
@@ -1,4 +1,14 @@
-from typing import Protocol
+from typing import Protocol, Callable, Any, Dict, Sequence
+
+class Context(Protocol):
+    runtime: "Context"
+    scope: "Context"
+    bots: Sequence
+    nonebot: "Context"
+    root: "Context"
+
+class Service(Protocol):
+    ctx: Context
 
 class ReggolLogger(Protocol):
     def success(self, obj, *args):
@@ -20,3 +30,6 @@ class NonebotJSLogger(ReggolLogger, Protocol):
     warning = ReggolLogger.warn
 
 logger: NonebotJSLogger
+unwrap: Callable[[Any], Any]
+config: Dict | Any
+caller: "Context"
