f69b00bc30c3ee223678b9b752ff5cca22e8df43
Cyan <68551684+CyanChanges@users.noreply.github.com>
2023-07-09 09:12:17 +0800

    chore: Update pyodide

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,7 +1,7 @@
-export const pyodideSource = 'https://ghproxy.com/https://github.com/pyodide/pyodide/releases/download/0.21.3/pyodide-build-0.21.3.tar.bz2'
+export const pyodideSource = 'https://ghproxy.com/https://github.com/pyodide/pyodide/releases/download/0.23.4/pyodide-build-0.23.4.tar.bz2'
 
-export const namePyyaml = 'PyYAML-6.0-cp310-cp310-emscripten_3_1_14_wasm32.whl'
+export const namePyyaml = 'PyYAML-6.0-cp311-cp311-emscripten_3_1_32_wasm32.whl'
 
-export const namePil = 'PIL-9.1.1-cp310-cp310-emscripten_3_1_14_wasm32.whl'
+export const namePil = 'PIL-9.1.1-cp311-cp311-emscripten_3_1_32_wasm32.whl
 
-export const nameNumpy = 'numpy-1.22.4-cp310-cp310-emscripten_3_1_14_wasm32.whl'
+export const nameNumpy = 'numpy-1.24.2-cp311-cp311-emscripten_3_1_32_wasm32.whl'
