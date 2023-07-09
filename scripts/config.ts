f45c7b5d30357e14a553f462f07bd7d5326156ee
Cyan <68551684+CyanChanges@users.noreply.github.com>
2023-07-09 09:16:13 +0800

    fix: forget to add symbol

--- scripts/config.ts
+++ scripts/config.ts
@@ -2,6 +2,6 @@ export const pyodideSource = 'https://ghproxy.com/https://github.com/pyodide/pyo
 
 export const namePyyaml = 'PyYAML-6.0-cp311-cp311-emscripten_3_1_32_wasm32.whl'
 
-export const namePil = 'PIL-9.1.1-cp311-cp311-emscripten_3_1_32_wasm32.whl
+export const namePil = 'PIL-9.1.1-cp311-cp311-emscripten_3_1_32_wasm32.whl'
 
 export const nameNumpy = 'numpy-1.24.2-cp311-cp311-emscripten_3_1_32_wasm32.whl'
